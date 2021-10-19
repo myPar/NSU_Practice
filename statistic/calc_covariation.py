fixed cov-matrices calculation; implemented cov-matrices view

statistic/calc_covariation.py
@@ -7,7 +7,7 @@ cov_matrices_names = ["all_with_all", "all_with_in", "all_with_out"]
 
 
 # get covaration matrices (input_data_dict - dict of pairs: key - attribute name, value - attribute data)
-def get_attributes_typical_cart(input_data_dict: dict, other_attributes_keys) -> dict:
+def get_attributes_typical_cart(input_data_dict, other_attributes_keys) -> dict:
     # order data: 1. input attr data 2. output attr data 3. other attr data
     data_dict = dict()
     # for input attr
@@ -36,8 +36,8 @@ def get_attributes_typical_cart(input_data_dict: dict, other_attributes_keys) ->
     count_all = len(input_data_dict)            # all attributes count
 
     # get from "all" cov-matrix "all with in" cov matrix:
-    delete_indices_row = np.arrange(count_in)                   # row indices to delete
-    delete_indices_column = np.arrange(count_in, count_all)     # column indices to delete
+    delete_indices_row = np.arange(count_in)                   # row indices to delete
+    delete_indices_column = np.arange(count_in, count_all)     # column indices to delete
 
     # delete input attr rows
     cov_matrix_all_in = np.delete(cov_matrix_all, delete_indices_row, 0)
@@ -46,7 +46,7 @@ def get_attributes_typical_cart(input_data_dict: dict, other_attributes_keys) ->
 
     # get from "all" cov-matrix "all with out" cov matrix:
     delete_indices_row = np.arange(count_in, count_in + count_out)
-    delete_indices_column = np.concatenate((np.arange(count_in), np.arrange(count_out, count_all)), 0)
+    delete_indices_column = np.concatenate((np.arange(count_in), np.arange(count_out, count_all)), 0)
     # delete output attr rows
     cov_matrix_all_out = np.delete(cov_matrix_all, delete_indices_row, 0)
     # delete not output columns